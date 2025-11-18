7c72aad4e0eb8aca471d98cf6e40590f7a61236f
kiwi <kiwi0911111254@gmail.com>
2025-11-18 16:01:30 +0800

    new

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Redirects
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 from catalog import views
 urlpatterns = [
@@ -28,3 +30,6 @@ urlpatterns = [
     path('catalog/books/add2/', views.add_book_model_form, name='add_book_model_form'),
     path('accounts/', include('accounts.urls')),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
